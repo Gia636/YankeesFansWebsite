8bf9316444ad73e13220eeb7bfd8aeaf0839312c
Gia636 <ge45@njit.edu>
2016-12-18 20:42:16 -0500

    Update YankeesFans.php

--- YankeesFans.php
+++ YankeesFans.php
@@ -1,24 +1,29 @@
 <?php
+//start the session each time
 ob_start();
 session_start();
+//connecting to my database
 require_once 'databaseconnect.php';
+//checking if the user is already logged in and if not redirecting to userlogin.php
  if( !isset($_SESSION['user']) ) {
   header("Location: userlogin.php");
   exit;
  }
+//this is where the user information is taken from
  $res=mysql_query("SELECT * FROM users WHERE userId=".$_SESSION['user']);
  $userRow=mysql_fetch_array($res);
+//including my design patterns file
 include_once 'designpatterns.php';
 $greeter = $userRow['userName'];
 $g = Greeter::create('Welcome ' . $greeter);
 $gr = $g->getGreeting(); 
 $a = new decorate;
-
+//connecting to my afs directory
 $target_dir = "/afs/cad.njit.edu/u/g/e/ge45/public_html/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-
+//uploading the file if the submit button is clicked and checking if it is an image
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
@@ -29,26 +34,25 @@ if(isset($_POST["submit"])) {
         $uploadOk = 0;
     }
 }
-
+//checking if the file already exists in the directory
 if (file_exists($target_file)) {
     echo "Please change the name of your file.";
     $uploadOk = 0;
 }
-
+//checking the size of the file
 if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Your file is too large.";
     $uploadOk = 0;
 }
-
+//checking the file type
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
     echo "This file type is not allowed. Only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
-
+//checking if the file was uploaded
 if ($uploadOk == 0) {
     echo "Error. Your file was not uploaded.";
-
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
@@ -56,10 +60,9 @@ if ($uploadOk == 0) {
         echo "There was an error uploading your file.";
     }
 }
-
-
-
+//ending the php and starting the html
 ?>
+
 <html>
 <body>
 <link rel="stylesheet" href="yankeescss.css">
